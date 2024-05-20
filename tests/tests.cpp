4e65a0e51ed76309535a1cde6eaa71a22af91c18
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-05-20 11:23:34 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,23 +1,44 @@
+#include <iostream>
 #include "Spider.h"
-#include <gtest/gtest.h>
+#include <gtest/gtest.h> // Пример использования Google Test
 
-TEST(SpiderTest, Initialization) {
+TEST(SpiderTest, InitTest) {
     Spider spider;
     spider.init("Black Widow", 100.0, 10.0);
     EXPECT_EQ(spider.isAlive(), true);
-    EXPECT_EQ(spider.created, 1);
 }
 
-TEST(SpiderTest, Attack) {
+TEST(SpiderTest, AttackTest) {
     Spider spider;
-    spider.init("Tarantula", 150.0, 20.0);
+    spider.init("Tarantula", 80.0, 15.0);
+    testing::internal::CaptureStdout();
     spider.attack();
-    EXPECT_GT(spider.hitpoints, 0.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Tarantula1 hits Player with 15 damage.\n");
 }
 
-TEST(SpiderTest, GetDamage) {
+TEST(SpiderTest, GetDamageTest) {
     Spider spider;
-    spider.init("Brown Recluse", 80.0, 15.0);
-    spider.getDamage(20);
-    EXPECT_LT(spider.hitpoints, 80.0);
+    spider.init("Jumping Spider", 50.0, 20.0);
+    testing::internal::CaptureStdout();
+    spider.getDamage(25);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Jumping Spider1 gets 25 damage.\n");
+    EXPECT_EQ(spider.isAlive(), true);
 }
+
+TEST(SpiderTest, DeathTest) {
+    Spider spider;
+    spider.init("Wolf Spider", 30.0, 5.0);
+    testing::internal::CaptureStdout();
+    spider.getDamage(35);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Wolf Spider1 gets 35 damage.\nWolf Spider1 dies\n");
+    EXPECT_EQ(spider.isAlive(), false);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
